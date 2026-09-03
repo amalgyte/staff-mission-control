ff95504ed19441e4e101224da56da8424033ea76
Simon Johnson <amalgyte@googlemail.com>
2026-09-03 16:07:09 +0100

    Centralize Mission Control OAuth client ID
    Load the public GitHub OAuth client ID from one cache-busted config file.

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * Staff Mission Control Configuration
+ * 
+ * GitHub OAuth Client ID for toggle persistence.
+ * Get one at https://github.com/settings/developers
+ */
+window.SMC_GITHUB_CLIENT_ID = 'Ov23livW2L1BX8IflcQ0';
